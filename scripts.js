67b2dc6ce6dd6707848544c3b3fe1328e15524bb
jibinjobyp <jibinjoby78@gmail.com>
2025-03-31 11:11:01 -0700

    Updated styles and animations for services and contact section

--- scripts.js
+++ scripts.js
@@ -1,26 +1,92 @@
 // JavaScript for the Slider Functionality
 function moveSlide(direction, sliderId) {
-    const slider = document.querySelector(`#${sliderId} .slider`);
-    const slides = slider.querySelectorAll('img');
-    const slideWidth = slides[0].clientWidth;
+  const slider = document.querySelector(`#${sliderId} .slider`);
+  const slides = slider.querySelectorAll("img");
+  const slideWidth = slides[0].clientWidth;
 
-    // Get current transform value
-    const currentTransform = getComputedStyle(slider).transform;
-    let currentTranslateX = currentTransform !== 'none' 
-        ? parseInt(currentTransform.split(',')[4]) 
-        : 0;
+  // Get current transform value
+  const currentTransform = getComputedStyle(slider).transform;
+  let currentTranslateX =
+    currentTransform !== "none" ? parseInt(currentTransform.split(",")[4]) : 0;
 
-    // Calculate new position
-    let newTranslateX = currentTranslateX + (direction * slideWidth);
+  // Calculate new position
+  let newTranslateX = currentTranslateX + direction * slideWidth;
 
-    // Looping the slides
-    if (newTranslateX < -(slideWidth * (slides.length - 1))) {
-        newTranslateX = 0;
-    } else if (newTranslateX > 0) {
-        newTranslateX = -(slideWidth * (slides.length - 1));
-    }
+  // Looping the slides
+  if (newTranslateX < -(slideWidth * (slides.length - 1))) {
+    newTranslateX = 0;
+  } else if (newTranslateX > 0) {
+    newTranslateX = -(slideWidth * (slides.length - 1));
+  }
 
-    // Apply the transform
-    slider.style.transition = "transform 0.5s ease-in-out";
-    slider.style.transform = `translateX(${newTranslateX}px)`;
+  // Apply the transform
+  slider.style.transition = "transform 0.5s ease-in-out";
+  slider.style.transform = `translateX(${newTranslateX}px)`;
 }
+
+
+gsap.registerPlugin(ScrollTrigger); 
+window.addEventListener("load", () => {
+  const tl = gsap.timeline();
+
+  // GSAP stagger animation for navbar links
+  gsap.from("nav a", {
+    opacity: 0,
+    y: -20,
+    duration: 0.8,
+    stagger: 0.2,
+    ease: "power2.out",
+  });
+
+  tl.fromTo(
+    ".top-right-circle",
+    {
+      opacity: 0,
+      y: 50,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      ease: "power2.out",
+    }
+  )
+  gsap.fromTo(".about-top-right-circle", {
+    opacity: 0,
+    y: 100,
+  },{
+    opacity:1,
+    y:0
+  });
+
+  gsap.fromTo('.mail,.mail-head',{
+    opacity:0,
+    x:-50
+  },
+{
+    opacity:1,
+    x:0
+})
+
+gsap.fromTo('.home-content h1,.home-content p',{
+    opacity:0,
+    y:50
+},{
+    opacity:1,
+    y:0,
+    duration:1,
+    ease:"power2.out"
+})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
